c17497b92ef8cbfad6372f972bb6df86660719b1
bshinn01 <bshinn@stevens.edu>
2022-03-02 11:23:36 -0500

    edited formatting of classify_triangle per pylint standards

--- classify_triangle.py
+++ classify_triangle.py
@@ -1,31 +1,33 @@
+"""
+This program contains a function, classify_triangle, which determines whether a triangle
+is equilateral, isosceles, or scalene, and whether the triangle is a right triangle
+"""
+
 import math
 
 
-def classify_triangle(a, b, c):
+def classify_triangle(a_side, b_side, c_side):
     """
     This function classifies triangles as equilateral, isosceles, and scalene, as well as
-    whether or not the triangle is a right triangle based on the side length params
+    whether the triangle is a right triangle based on the side length params
 
-    :param a: side length of triangle
-    :param b: side length of triangle
-    :param c: side length of triangle
+    :param a_side: side length of triangle
+    :param b_side: side length of triangle
+    :param c_side: side length of triangle
     :return: 'Equilateral', 'Isosceles', or 'Scalene', and 'Right' or 'Not Right'
     """
-    longest_side = max(a, b, c)
-    other_sides = [a, b, c]
+    longest_side = max(a_side, b_side, c_side)
+    other_sides = [a_side, b_side, c_side]
     other_sides.remove(longest_side)
-    if (a + b <= c) or (a + c <= b) or (c + b <= a):
+    if (a_side + b_side <= c_side) or (a_side + c_side <= b_side) or (c_side + b_side <= a_side):
         return "Error: Invalid side lengths for triangle."
-    elif int(math.pow(longest_side, 2)) == math.pow(other_sides[0], 2) + math.pow(other_sides[1], 2):
-        if (a == b) and (b == c):
-            return "Equilateral, Right"
-        elif (a != b) and (b != c) and (a != c):
+    if int(math.pow(longest_side, 2)) == math.pow(other_sides[0], 2) + \
+            math.pow(other_sides[1], 2):
+        if (a_side != b_side) and (b_side != c_side) and (a_side != c_side):
             return "Scalene, Right"
-        else:
-            return "Isosceles, Right"
-    elif (a == b) and (b == c):
+        return "Isosceles, Right"
+    if (a_side == b_side) and (b_side == c_side):
         return "Equilateral, Not Right"
-    elif (a != b) and (b != c) and (a != c):
+    if (a_side != b_side) and (b_side != c_side) and (a_side != c_side):
         return "Scalene, Not Right"
-    else:
-        return "Isosceles, Not Right"
+    return "Isosceles, Not Right"
